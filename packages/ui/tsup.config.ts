8132f34024855bc8080f7f86689e4a5e67fff0d4
Faridul Islam <foridpathan45@gmail.com>
2025-10-18 17:57:13 +0600

    fix: disable sourcemap to reduce package size
    Signed-off-by: Faridul Islam <foridpathan45@gmail.com>

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
-  sourcemap: true,
+  sourcemap: false, // Disabled to reduce package size (was adding 1.5MB)
   clean: true,
   external: ['react', 'react-dom'],
   target: 'es5', 
